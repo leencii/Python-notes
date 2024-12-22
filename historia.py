8ddfc081407ad40ae4c4335ad9e2162e085da6bc
len <lencinathomas819@gmail.com>
2024-12-22 01:56:32 -0300

    Add files via upload

---
+++ historia.py
@@ -0,0 +1,14 @@
+print ("te encuentras en un bosque solo y sin rumbo, te cruzas con un lobo hambriento el cual te acecha desde lejos, tienes dos opciones correr o lanzarle una roca")
+eleccion = input ("elige entre \"correr\" y \"lanzar\" la roca aqui:") 
+if eleccion == "correr":
+ print ("el lobo te sigue y terminas siendo devorado por este") 
+if eleccion == "lanzar":
+ print ("al lanzarle la roca, el lobo la atrapa con su mano y te mira confundido, suelta el objeto y este comienza a comunicarse, siendo su primer palabra \"hola humano\"")
+eleccion = input ("para volver escribe \"volver\":")
+if eleccion == "volver":
+ print ("te encuentras en un bosque solo y sin rumbo, te cruzas con un lobo hambriento el cual te acecha desde lejos, tienes dos opciones correr o lanzarle una roca")
+ eleccion = input ("elige entre \"correr\" y \"lanzar\" la roca aqui:") 
+if eleccion == "correr":
+ print ("el lobo te sigue y terminas siendo devorado por este") 
+if eleccion == "lanzar":
+ print ("al lanzarle la roca, el lobo la atrapa con su mano y te mira confundido, suelta el objeto y este comienza a comunicarse, siendo su primer palabra \"hola humano\"")
