d2443dff7b2abed54ce28d86e7831ccb540fcfac
len <lencinathomas819@gmail.com>
2025-02-11 05:46:50 -0300

    Add files via upload

---
+++ Udemy practice.py
@@ -0,0 +1,13 @@
+autos_azules = 3 
+autos_rojos = 4 
+autos_amarillos = 6 
+
+cantidad_de_autos = (autos_azules + autos_rojos + autos_amarillos)
+print(cantidad_de_autos)
+print(type(autos_azules), type(autos_rojos), type(autos_amarillos))
+
+mood = "comando"
+strength = 17.2
+rank = 12
+
+print(type(mood), type(strength), type(rank))
